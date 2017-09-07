78a7a2fc3ddd7f5af305b5890cd24311febbc03d
Water <vadimka4552015@gmail.com>
2017-09-07 19:22:21 +0300

    new commit (test)

---
+++ src/sample/Constans.java
@@ -0,0 +1,10 @@
+package sample;
+
+import java.util.Locale;
+
+class Constans {
+    static final Locale RUS=new Locale.Builder().setLanguage("ru").build();
+    static final Locale ES_HN= new Locale.Builder().setLanguage("es").setRegion("HN").build();
+    static final Locale NL= new Locale.Builder().setLanguage("nl").build();
+    static final Locale CAT= new Locale.Builder().setLanguage("cat").build();
+}
